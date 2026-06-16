becaad80218a4363d95e8017f4346ef28552e96e
Gilles <gilles.ienne@free.fr>
2026-06-16 11:28:42 +0200

    feat: Input struct — brandSpecificInstId/PkgId + container dimensions (keys 5,6,42-45)

--- internal/openprinttag/codec.go
+++ internal/openprinttag/codec.go
@@ -489,6 +489,8 @@ func (i *Input) ToOpenPrintTag() (*OpenPrintTag, error) {
 	opt.Main.NominalNettoFullWeight = i.NominalWeight
 	opt.Main.FilamentDiameter = i.FilamentDiameter
 	opt.Main.Density = i.Density
+	opt.Main.BrandSpecificInstanceID = i.BrandSpecificInstID
+	opt.Main.BrandSpecificPackageID = i.BrandSpecificPkgID
 	opt.Main.ActualNettoFullWeight = i.ActualWeight
 	opt.Main.EmptyContainerWeight = i.SpoolWeight
 	opt.Main.MinPrintTemp = i.MinPrintTemp
@@ -498,6 +500,10 @@ func (i *Input) ToOpenPrintTag() (*OpenPrintTag, error) {
 	opt.Main.MaxBedTemp = i.MaxBedTemp
 	opt.Main.MinChamberTemp = i.MinChamberTemp
 	opt.Main.ChamberTemp = i.ChamberTemp
+	opt.Main.ContainerWidth = i.ContainerWidth
+	opt.Main.ContainerOuterDiameter = i.ContainerOuterDiam
+	opt.Main.ContainerInnerDiameter = i.ContainerInnerDiam
+	opt.Main.ContainerHoleDiameter = i.ContainerHoleDiam
 	opt.Main.NominalFullLength = i.NominalFullLength
 	opt.Main.ActualFullLength = i.ActualFullLength
 	opt.Main.ManufacturedDate = i.ManufacturedDate
